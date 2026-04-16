1a4dba05082d98e1bca7fd2b323836bed98f0d87
imba97 <mail@imba97.cn>
2026-04-16 11:53:18 +0800

    chore: add typed-router.d.ts to gitignore

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import UnoCSS from "@unocss/vite";
 import Vue from "@vitejs/plugin-vue";
+import { dirname } from "path";
+import { fileURLToPath, resolve } from "url";
 import { defineConfig } from "vite-plus";
 import VueRouter from "vue-router/vite";
 
+const r = (p: string) => resolve(dirname(fileURLToPath(import.meta.url)), p);
+
 export default defineConfig({
-  plugins: [VueRouter(), Vue(), UnoCSS()],
+  plugins: [
+    VueRouter({
+      dts: r(".auto-generated/typed-router.d.ts"),
+    }),
+    Vue(),
+    UnoCSS(),
+  ],
 });
