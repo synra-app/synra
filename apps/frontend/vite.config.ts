9ee87bfd27e4cb9e2470b1d9b9aeda65a8021bb7
imba97 <mail@imba97.cn>
2026-04-17 00:12:39 +0800

    fix: type error

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,18 +1,15 @@
 import UnoCSS from "@unocss/vite";
 import Vue from "@vitejs/plugin-vue";
-import { dirname } from "path";
 import { fileURLToPath, resolve } from "url";
-import { defineConfig } from "vite-plus";
+import { defineConfig, type PluginOption } from "vite-plus";
 import VueRouter from "vue-router/vite";
 
-const r = (p: string) => resolve(dirname(fileURLToPath(import.meta.url)), p);
+const r = (p: string) => resolve(fileURLToPath(import.meta.url), p);
 
 export default defineConfig({
   plugins: [
-    VueRouter({
-      dts: r(".auto-generated/typed-router.d.ts"),
-    }),
-    Vue(),
-    UnoCSS(),
+    VueRouter({ dts: r(".auto-generated/typed-router.d.ts") }) as unknown as PluginOption,
+    Vue() as unknown as PluginOption,
+    UnoCSS() as unknown as PluginOption,
   ],
 });
